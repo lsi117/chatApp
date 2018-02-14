a4a5fc04320bba68398d85fd260d19688ba541ea
lauren inacio <lauren_f23@yahoo.com>
2018-02-14 14:29:26 -0500

    working on establishing connection

--- chatapp/src/server/index.js
+++ chatapp/src/server/index.js
@@ -3,10 +3,10 @@ var io = module.exports.io = require('socket.io')(app)
 
 const PORT = process.env.PORT || 3231
 
-const SocketManager = require('./SocketManager.js')
+const SocketManager = require('./SocketManager')
 
 io.on('connection', SocketManager)
 
 app.listen(PORT, ()=>{
-  console.log("Connection successful");
+  console.log("Connection to port: " + PORT);
 })

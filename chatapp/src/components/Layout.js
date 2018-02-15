3fb807de62deb6aa3f7e083b0dcde619bcd2cb0e
lauren inacio <lauren_f23@yahoo.com>
2018-02-15 16:04:02 -0500

    import eventsjs into layout

--- chatapp/src/components/Layout.js
+++ chatapp/src/components/Layout.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import io from 'socket.io-client';
+import {USER_CONNECTED} from '../Events';
 
 const socketUrl = "http://192.168.1.102:3231"
 export default class Layout extends Component{

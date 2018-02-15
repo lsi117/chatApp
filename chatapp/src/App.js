a038245e42acc6758a9229b78aca5557c66ff3ee
lauren inacio <lauren_f23@yahoo.com>
2018-02-15 13:50:10 -0500

    added component to app

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Layout />
+        <Layout title="Chat Application"/>
       </div>
     );
   }

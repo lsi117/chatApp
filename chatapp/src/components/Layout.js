d30a4fe1f508d17f5a061b37a1ea89649714ee25
lauren inacio <lauren_f23@yahoo.com>
2018-02-15 14:21:23 -0500

    add constructor to layout comp

--- chatapp/src/components/Layout.js
+++ chatapp/src/components/Layout.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 
 export default class Layout extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {};
+  }
+
   render(){
     const {title} = this.props
     return(
